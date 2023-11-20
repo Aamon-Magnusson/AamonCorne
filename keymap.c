caf5d5f809b7f0a66fb6b56e5b7a8023c6d590d2
AMANM6 <aamonm6@gmail.com>
2023-11-20 09:38:44 -0700

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -14,7 +14,7 @@ enum custom_keycodes {
         WINDOW_1,
         WINDOW_2,
         WINDOW_4,
-        WINDOW_6
+        WINDOW_6,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -54,7 +54,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         if (record->event.pressed) {
                                 //del_mods(current_mods);
                                 for (int i = 0; i < 9; i++){
-                                        SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_LEFT))));
+									SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_LEFT))));
                                 }
                                 //add_mods(current_mods);
                         }
@@ -64,7 +64,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         if (record->event.pressed) {
                                 //del_mods(current_mods);
                                 for (int i = 0; i < 9; i++){
-                                        SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_LEFT))));
+									SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_LEFT))));
                                 }
                                 SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_RIGHT))));
                                 //add_mods(current_mods);
@@ -75,7 +75,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         if (record->event.pressed) {
                                 //del_mods(current_mods);
                                 for (int i = 0; i < 9; i++){
-                                        SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_LEFT))));
+									SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_LEFT))));
                                 }
                                 SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_RIGHT))));
                                 SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_RIGHT))));
@@ -87,7 +87,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         if (record->event.pressed) {
                                 //del_mods(current_mods);
                                 for (int i = 0; i < 9; i++){
-                                        SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_LEFT))));
+									SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_LEFT))));
                                 }
                                 SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_RIGHT))));
                                 SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_RIGHT))));
@@ -100,7 +100,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         if (record->event.pressed) {
                                 //del_mods(current_mods);
                                 for (int i = 0; i < 9; i++){
-                                        SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_LEFT))));
+									SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_LEFT))));
                                 }
                                 SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_RIGHT))));
                                 SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_RIGHT))));
@@ -117,28 +117,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 case WINDOW_1:
                         if (record->event.pressed) {
                                 SEND_STRING(SS_LGUI(SS_TAP(X_Z)));
-                                _delay_ms(250);
+                                //_delay_ms(250);
                                 SEND_STRING("11");
                         }
                         break;
                 case WINDOW_2:
                         if (record->event.pressed) {
                                 SEND_STRING(SS_LGUI(SS_TAP(X_Z)));
-                                _delay_ms(250);
+                                //_delay_ms(250);
                                 SEND_STRING("21");
                         }
                         break;
                 case WINDOW_4:
                         if (record->event.pressed) {
                                 SEND_STRING(SS_LGUI(SS_TAP(X_Z)));
-                                _delay_ms(250);
+                                //_delay_ms(250);
                                 SEND_STRING("41");
                         }
                         break;
                 case WINDOW_6:
                         if (record->event.pressed) {
                                 SEND_STRING(SS_LGUI(SS_TAP(X_Z)));
-                                _delay_ms(250);
+                                //_delay_ms(250);
                                 SEND_STRING("62");
                         }
                         break;
@@ -148,51 +148,51 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_split_3x6_3(
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
-        LT(6,KC_ESC), MT(MOD_LCTL, KC_A), MT(MOD_LSFT, KC_S), MT(MOD_LALT, KC_D), MT(MOD_LGUI, KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        MT(MOD_LCTL, KC_LGUI), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N,MT(MOD_RGUI, KC_M), MT(MOD_RALT, KC_COMM),MT(MOD_RSFT, KC_DOT), MT(MOD_RCTL, KC_SLSH),   TO(3),
+        LT(3,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+        LT(4,KC_ESC), MT(MOD_LCTL, KC_A), MT(MOD_LSFT, KC_S), MT(MOD_LALT, KC_D), MT(MOD_LGUI, KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        MT(MOD_LCTL, KC_LGUI), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N,MT(MOD_RGUI, KC_M), MT(MOD_RALT, KC_COMM),MT(MOD_RSFT, KC_DOT), MT(MOD_RCTL, KC_SLSH), MO(3),
         KC_DEL,LT(2,KC_ENT), MO(1), LT(1,KC_SPC), KC_RSFT, KC_BSPC),
 
 // VIM layer
 [1] = LAYOUT_split_3x6_3(
-        KC_TRNS, DM_REC1, C(KC_RIGHT), KC_TRNS, KC_TRNS, KC_TRNS, C(KC_C), C(KC_Z), KC_HOME, VIM_O, C(KC_V), KC_PGUP,
-        KC_TRNS,MT(MOD_LCTL, KC_END),MT(MOD_LSFT, DM_RSTP),MT(MOD_LALT, KC_TRNS),MT(MOD_LGUI, KC_TRNS), VIM_G, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_PGDN,
-        TO(0), VIM_QUIT, C(KC_X), KC_TRNS, KC_TRNS, C(KC_LEFT), KC_TRNS, DM_PLY1, KC_TRNS, KC_TRNS, C(KC_F), KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+        KC_TAB, KC_NO, C(KC_RIGHT), KC_NO, KC_NO, KC_NO, C(KC_C), C(KC_Z), KC_HOME, VIM_O, C(KC_V), KC_PGUP,
+        KC_ESC,MT(MOD_LCTL, KC_END), KC_LSFT, KC_LALT, KC_LGUI, VIM_G, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_PGDN,
+        KC_LCTL, VIM_QUIT, C(KC_X), KC_NO, KC_NO, C(KC_LEFT), KC_NO, KC_NO, KC_NO, KC_NO, C(KC_F), KC_NO,
+        KC_DEL, KC_ENT, KC_NO, KC_SPC, KC_RSFT, KC_BSPC),
 
 // Numbers and special chars
 [2] = LAYOUT_split_3x6_3(
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PSCR,
-        KC_TRNS,MT(MOD_LCTL, KC_LPRN),MT(MOD_LSFT, KC_RPRN),MT(MOD_LALT, KC_LBRC),MT(MOD_LGUI, KC_RBRC), KC_MINS, KC_DOT, KC_4, KC_5, KC_6, KC_ASTR, KC_SLSH,
-        TO(0), KC_LT, KC_GT, KC_LCBR, KC_RCBR, KC_EQL, KC_0, KC_1, KC_2, KC_3, KC_PLUS, KC_MINS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+        KC_NO,MT(MOD_LCTL, KC_LPRN),MT(MOD_LSFT, KC_RPRN),MT(MOD_LALT, KC_LBRC),MT(MOD_LGUI, KC_RBRC), KC_MINS, KC_DOT, KC_4, KC_5, KC_6, KC_ASTR, KC_SLSH,
+        KC_LCTL, KC_LT, KC_GT, KC_LCBR, KC_RCBR, KC_EQL, KC_0, KC_1, KC_2, KC_3, KC_PLUS, KC_MINS,
+        KC_DEL, KC_ENT, KC_NO, KC_SPC, KC_RSFT, KC_BSPC),
 
 // Mouse and system
 [3] = LAYOUT_split_3x6_3(
-        KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_TRNS, KC_TRNS,
-        TO(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(5), TO(4),
-        KC_TRNS, KC_BTN2, KC_BTN1, KC_MPLY, KC_MUTE, KC_TRNS),
-
-// F-keys
-[4] = LAYOUT_split_3x6_3(
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-        KC_TRNS, KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_F12,
-        TO(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RGUI, KC_RALT, KC_RSFT, KC_RCTL, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-
-// Gaming layer (Currently LT don't work)
-[5] = LAYOUT_split_3x6_3(
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ESC,
-        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TO(0),
-        KC_LALT, LT(2,KC_ENT), KC_SPC, LT(1,KC_SPC), KC_RSFT, KC_BSPC),
+        KC_NO, KC_NO, KC_MS_U, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_NO,
+       	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_BTN2, KC_BTN1, KC_MPLY, KC_MUTE, KC_NO),
 
 // Tag layer
-[6] = LAYOUT_split_3x6_3(
-        G(KC_TAB), TAG_1, TAG_2, TAG_3, TAG_4, TAG_5, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, G(C(KC_LEFT)), KC_TRNS,
-        KC_TRNS, WINDOW_1, WINDOW_2, WINDOW_4, WINDOW_6, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        TO(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, G(C(KC_RIGHT)), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, TEST_EMOJI, KC_TRNS, KC_TRNS, KC_TRNS),
+[4] = LAYOUT_split_3x6_3(
+        G(KC_TAB), TAG_1, TAG_2, TAG_3, TAG_4, TAG_5, KC_NO, KC_NO, KC_NO, KC_NO, G(C(KC_LEFT)), KC_NO,
+        KC_NO, WINDOW_1, WINDOW_2, WINDOW_4, WINDOW_6, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, G(C(KC_RIGHT)), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+
+// F-keys
+//[4] = LAYOUT_split_3x6_3(
+//        KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+//        KC_NO, KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_F12,
+//        TO(0), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RGUI, KC_RALT, KC_RSFT, KC_RCTL, KC_NO,
+//        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+
+// Gaming layer (Currently LT don't work)
+//[5] = LAYOUT_split_3x6_3(
+//        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ESC,
+//        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+//        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TO(0),
+//        KC_LALT, KC_ESC, KC_SPC, KC_SPC, KC_RSFT, KC_BSPC),
 
 };
